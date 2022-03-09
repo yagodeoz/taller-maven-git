b6dec3cd7d4ac6a68721f323b31186b453d59d73
Byron <byronsantiago@gmail.com>
2022-03-08 20:02:45 -0500

    Mi primera clase

---
+++ taller-maven-git/src/main/java/com/estrategit/curso/Persona.java
@@ -0,0 +1,16 @@
+package com.estrategit.curso;
+
+/*
+ * @Autor: Byron Segovia
+ */
+
+//Clase de pruebas taller 5
+public class Persona {
+
+	public static void main(String[] argumentos) {
+
+		System.out.print("Hola");
+
+	}
+
+}
