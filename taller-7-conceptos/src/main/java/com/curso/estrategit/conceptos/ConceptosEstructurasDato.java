f27a631199bef4be467e54e1e99c34a3a030fca3
Byron <byronsantiago@gmail.com>
2022-03-16 18:46:35 -0500

    Clases HashMap

--- taller-7-conceptos/src/main/java/com/curso/estrategit/conceptos/ConceptosEstructurasDato.java
+++ taller-7-conceptos/src/main/java/com/curso/estrategit/conceptos/ConceptosEstructurasDato.java
@@ -1,7 +1,9 @@
 package com.curso.estrategit.conceptos;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Vector;
 
 
 public class ConceptosEstructurasDato {
@@ -33,6 +35,35 @@ public class ConceptosEstructurasDato {
 			System.out.println(recorre.next());
 		}
 		
+		Vector<String> vector = new Vector<String>();
+		
+		vector.add("Cadena1");
+		vector.add( 0, "Cadena2");
+		vector.add( 0, "Cadena8");
+		
+		for (String elem : vector) {
+			System.out.println(elem);
+		}
+		
+		HashMap<String, Long> edadPersona = 
+				              new HashMap<String, Long>();
+		
+		edadPersona.put("ByronSegovia", new Long(39));
+		edadPersona.put("JudithSegovia", new Long(14));
+		edadPersona.put("SadithSegovia", new Long(5));
+		edadPersona.put("MicaelaSegovia", new Long(2));
+		
+		Iterator<String> recorreClaves = 
+				edadPersona.keySet().iterator();
+		
+		while (recorreClaves.hasNext()) {
+			String clavePrimaria = recorreClaves.next();
+			System.out.println("La persona: " 
+			                   + clavePrimaria 
+			                   + ", tiene: " 
+			                   + edadPersona.get(clavePrimaria)
+			                   + " años"); 
+		}
 		
 		
 		
