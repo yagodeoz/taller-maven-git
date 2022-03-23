ba55469fb50348792919e23f60289597a024e1eb
Byron <byronsantiago@gmail.com>
2022-03-23 18:11:30 -0500

    Base datos - Resultset

--- taller-12-jdbc/src/main/java/com/estrategit/curso/logicabd/PruebasSentenciasDML.java
+++ taller-12-jdbc/src/main/java/com/estrategit/curso/logicabd/PruebasSentenciasDML.java
@@ -1,6 +1,7 @@
 package com.estrategit.curso.logicabd;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.estrategit.curso.singleton.ConexionBaseDatos;
@@ -10,6 +11,19 @@ public class PruebasSentenciasDML {
 	public static void main(String[] args) {
 		try {
 			Connection conexion = ConexionBaseDatos.getConexionSqlServer();
+			
+			ResultSet resultado = conexion.createStatement()
+					              .executeQuery("select * from CURSO_JAVA");
+			while (resultado.next()) {
+				String nombre = resultado.getString("nombre");
+				String apellido = resultado.getString("apellido");
+				String id = resultado.getString("id");
+				
+				
+			}
+			
+			
+			
 		} catch (SQLException e) {
 			System.err.println("Error al crear la conexión");
 		}
